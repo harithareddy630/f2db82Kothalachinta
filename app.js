e128f1de8a3b47a0e266fc1dcac3e30e9ea18e30
harithareddy630 <haritha.rdy630@gmail.com>
2022-11-18 01:18:41 -0600

    files updated

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;  
 require('dotenv').config();
 const connectionString =
 process.env.MONGO_CON
@@ -34,8 +35,23 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bridges', bridgeRouter);  
 app.use('/gridbuild', gridbuildRouter); 
-app.use('/selector', selectorRouter);  
+app.use('/selector', selectorRouter); 
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session());   
 app.use('/resource', resourceRouter); 
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser());   
 
 // We can seed the collection if needed on server start 
 async function recreateDB(){ 
