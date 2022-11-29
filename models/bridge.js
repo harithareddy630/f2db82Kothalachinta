56db96d8662fd64f8c9a3215e7cbb858b8772d96
harithareddy630 <haritha.rdy630@gmail.com>
2022-11-28 22:54:57 -0600

    updated files

--- models/bridge.js
+++ models/bridge.js
@@ -1,11 +1,18 @@
-const mongoose = require("mongoose") 
-const bridgeSchema = mongoose.Schema({ 
- bridge_type: String,     
- bridge_name: String, 
- bridge_size: Number  
-}) 
- 
-module.exports = mongoose.model("bridge", 
-bridgeSchema)  
-
- 
+const mongoose = require("mongoose")
+const bridgeSchema = mongoose.Schema({
+    bridge_name: {
+        type:String,
+        required:true
+    },
+    bridge_type: {
+        type:String,  
+        required:true
+    },
+    bridge_size: {
+        type:Number,  
+        min:1,
+        max:1000   
+    }
+})
+module.exports = mongoose.model("bridge",
+    bridgeSchema)   
--- routes/bridge.js
+++ routes/bridge.js
@@ -3,6 +3,16 @@ const bridge_controlers= require('../controllers/bridge');
 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('bridge', { title: 'Serach Results Bridge' });
@@ -15,11 +25,11 @@ router.get('/', bridge_controlers.bridge_view_all_Page );
 module.exports = router;
 
 /* GET create bridge page */ 
-router.get('/create', bridge_controlers.bridge_create_Page); 
+router.get('/create',secured, bridge_controlers.bridge_create_Page); 
 /* GET create update page */ 
-router.get('/update', bridge_controlers.bridge_update_Page); 
+router.get('/update',secured, bridge_controlers.bridge_update_Page); 
 /* GET delete bridge page */ 
-router.get('/delete', bridge_controlers.bridge_delete_Page); 
+router.get('/delete',secured, bridge_controlers.bridge_delete_Page); 
 // GET request for one bridge. 
 router.get('/bridges/:id', bridge_controlers.bridge_detail); 
-module.exports = router; 
+module.exports = router;  
